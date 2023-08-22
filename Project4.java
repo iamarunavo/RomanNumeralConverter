79d4d647ce8d5e8f3d8a57913bde367ff17a4e51
iamarunavo <101681316+iamarunavo@users.noreply.github.com>
2023-08-22 17:56:21 -0400

    Add files via upload

---
+++ Project4.java
@@ -0,0 +1,10 @@
+/**
+ * The main method that calls my GUI when ran
+ * @author Arunavo Chowdhury
+ * Lab Section 22B
+ */
+public class Project4 {
+	public static void main (String args[]) {
+		RomanNumeralGUI gui = new RomanNumeralGUI(); //to call my GUI when ran
+	}
+}
